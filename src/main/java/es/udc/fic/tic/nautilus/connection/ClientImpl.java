624629ec6e36e1addc4ab5e961b21d288c70a85c
luislorenzom <luislorenzomosquera@gmail.com>
2016-02-10 01:05:42 +0100

    Se ha corregido un bug que enviaba los ficheros a un servidor aunque este no existiese, siempre y cuando se hubiese enviado un fichero antes a un servidor válido.
    Añadido un contador, que prueba a enviar un archivo, que previamente
    había fallada, antes de intentar probar a enviar el archivo a otro host.
    
    Añadida la opción que limpia todos los archivos temporales si la
    operación de guardado de un archivo falla.

--- src/main/java/es/udc/fic/tic/nautilus/connection/ClientImpl.java
+++ src/main/java/es/udc/fic/tic/nautilus/connection/ClientImpl.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Random;
 
 import net.tomp2p.connection.Bindings;
-import net.tomp2p.connection.DiscoverNetworks;
+//import net.tomp2p.connection.DiscoverNetworks;
 import net.tomp2p.futures.FutureBootstrap;
 import net.tomp2p.futures.FutureDirect;
 import net.tomp2p.futures.FutureDiscover;
@@ -51,12 +51,27 @@ public class ClientImpl implements Client {
 		int index = 0;
 		
 		for (NautilusMessage msg : msgs) {
+			boolean clean = true;
 			List<String> serverPreferences = connectionUtilities.getHostAndBackupFromConfig();
 			for (String host : serverPreferences) {
 				//String host = serverPreferences.get(0);
-				int result = startClient(host, msg);
+				System.out.println("Save the file");
+				
+				// Make three intends
+				int result = 0;
+				for (int j = 0; j < 3; j++) {
+					result = startClient(host, msg);
+					
+					if (result == 1) {
+						break;
+					}
+					//TODO: añadir un wait en cada uno de los 
+					//intentos para dar un margen de tiempo?
+				}
 				
 				if (result == 1) {
+					clean = false;
+					System.out.println("ok!");
 					// Save the host in the key file
 					nautilusKey.get(index).setHost(host);
 					
@@ -72,10 +87,21 @@ public class ClientImpl implements Client {
 						int tmpIndex = 0;
 						for (String hostBackup : serverPreferences) {
 							if (hostBackup != host) {
-								int resultMirroring = startClient(hostBackup, msg);
+								System.out.println("Mirroring the file");
+								
+								// Make three intends
+								int resultMirroring = 0;
+								for (int j = 0; j < 3; j++) {
+									resultMirroring = startClient(hostBackup, msg);
+									
+									if (resultMirroring == 1) {
+										break;
+									}
+								}
 								
 								if (resultMirroring == 1) {
 									// Success, now save the hostBackup in the keyFile
+									System.out.println("ok!");
 									nautilusKey.get(index).setHostBackup(hostBackup);
 									break;
 								}
@@ -93,6 +119,14 @@ public class ClientImpl implements Client {
 				} 
 			}
 			
+			if (clean) {
+				// if can't sent an file split then delete the 
+				// key and the encrypt files
+				System.out.println("Can't sent one file split. Cleaning the tmp files");
+				cleanAllTmpFile(filePath);
+				System.exit(0);
+			}
+			
 			index++;
 		}
 		keysHandler.generateKeys(nautilusKey);
@@ -106,7 +140,16 @@ public class ClientImpl implements Client {
 		
 		for (NautilusKey key : keys) {
 			NautilusMessage msg = new NautilusMessage(0, key.getHash());
-			int result = startClient(key.getHost(), msg);
+			
+			// Make three intends
+			int result = 0;
+			for (int j = 0; j < 3; j++) {
+				result = startClient(key.getHost(), msg);
+				
+				if (result == 1) {
+					break;
+				}
+			}
 			
 			if (result == 1) {
 				File file = new File(key.getHash()+".aes256");
@@ -118,7 +161,16 @@ public class ClientImpl implements Client {
 			} else {
 				// Make the petition to the backup host
 				if (key.getHostBackup() != null) {
-					int secondResult = startClient(key.getHostBackup(), msg);
+					
+					// Make three intends
+					int secondResult = 0;
+					for (int j = 0; j < 3; j++) {
+						secondResult = startClient(key.getHostBackup(), msg);
+						
+						if (secondResult == 1) {
+							break;
+						}
+					}
 					
 					if (secondResult == 1) {
 						File file = new File(key.getHash()+".aes256");
@@ -146,103 +198,118 @@ public class ClientImpl implements Client {
 	/*********************/
 	
 	private int startClient(String ipAddress, NautilusMessage msgObject) throws Exception {
-		Random rnd = new Random(42L);
-		Bindings b = new Bindings().listenAny();
-		Peer client = new PeerBuilder(new Number160(rnd)).ports(4001).bindings(b).start();
-		System.out.println("Client started and Listening to: " + DiscoverNetworks.discoverInterfaces(b));
-		System.out.println("address visible to outside is " + client.peerAddress());
-
-		InetAddress address = Inet4Address.getByName(ipAddress);
-		int masterPort = 4000;
-		PeerAddress pa = new PeerAddress(Number160.ZERO, address, masterPort, masterPort, masterPort +1 );
-
-		System.out.println("PeerAddress: " + pa);
-		
-		// Future Discover
-		FutureDiscover futureDiscover = client.discover().expectManualForwarding().inetAddress(address).ports(masterPort).start();
-		futureDiscover.awaitUninterruptibly();
-
-		// Future Bootstrap - slave
-		FutureBootstrap futureBootstrap = client.bootstrap().inetAddress(address).ports(masterPort).start();
-		futureBootstrap.awaitUninterruptibly();
-
-		Collection<PeerAddress> addressList = client.peerBean().peerMap().all();
-		System.out.println("=====> "+addressList.size());
-		
-		// if can not connect with the server
-		if (addressList.size() == 0) {
+		try {
+			System.out.println(ipAddress +" "+msgObject.getHash());
+			Random rnd = new Random(42L);
+			Bindings b = new Bindings().listenAny();
+			Peer client = new PeerBuilder(new Number160(rnd)).ports(4001).bindings(b).start();
+			//System.out.println("Client started and Listening to: " + DiscoverNetworks.discoverInterfaces(b));
+			//System.out.println("address visible to outside is " + client.peerAddress());
+	
+			InetAddress address = Inet4Address.getByName(ipAddress);
+			int masterPort = 4000;
+			//PeerAddress pa = new PeerAddress(Number160.ZERO, address, masterPort, masterPort, masterPort +1 );
+	
+			//System.out.println("PeerAddress: " + pa);
+			
+			// Future Discover
+			FutureDiscover futureDiscover = client.discover().expectManualForwarding().inetAddress(address).ports(masterPort).start();
+			futureDiscover.awaitUninterruptibly();
+	
+			// Future Bootstrap - slave
+			FutureBootstrap futureBootstrap = client.bootstrap().inetAddress(address).ports(masterPort).start();
+			futureBootstrap.awaitUninterruptibly();
+	
+			Collection<PeerAddress> addressList = client.peerBean().peerMap().all();
+			System.out.println("=====> "+addressList.size());
+			
+			// if can not connect with the server
+			if (addressList.size() == 0) {
+				client.shutdown();
+				return -1;
+			}
+			
+			// patch for the fake ip
+			if (!(addressList.iterator().next().inetAddress().getHostAddress().equals(ipAddress))) {
+				//System.out.println("here!!");
+				client.shutdown();
+				return -1;
+			}
+			
+			
+			if (futureDiscover.isSuccess()) {
+				System.out.println("found that my outside address is " + futureDiscover.peerAddress());
+			} else {
+				// Send the file without problem!
+				System.out.println("failed 1 " + futureDiscover.failedReason());
+			}
+			
+			PeerAddress peerA = addressList.iterator().next();
+			
+			byte[] msg = objectToByteArray(msgObject);
+			
+			
+			if (msgObject.getType() == 1) {
+				// Send and logic to process msg type one
+				FutureDirect future = client.sendDirect(peerA).object(msg).start();
+				
+				future.awaitUninterruptibly();
+				
+				if (future.isSuccess()) {
+					
+					System.out.println("=====> receiving message");
+					int val = (int) future.object();
+					if (val == 1) {
+						// Success!!
+						System.out.println("=====> File part correctly sent");
+						client.shutdown();
+						return val;
+					} else {
+						// Fail in the server (can't save for space, permits, doesn't find, etc)
+						System.out.println("=====> has been some error in the server");
+						client.shutdown();
+						return val;
+					}
+					
+				} else {
+					System.out.println("failed 2 " + future.failedReason());
+				}
+			}
+			
+			if (msgObject.getType() == 0) {
+				// Send and logic to process msg type zero
+				FutureDirect future = client.sendDirect(peerA).object(msg).start();
+				
+				future.awaitUninterruptibly();
+				
+				if (future.isSuccess()) {
+					System.out.println("=====> receiving message");
+					try{
+						byte[] byteArray = (byte[]) future.object();
+						FileOutputStream fos = new FileOutputStream(msgObject.getHash()+".aes256");
+						fos.write(byteArray);
+						fos.close();
+						
+						System.out.println("=====> File part recovered");
+						client.shutdown();
+						return 1;
+					} catch (Exception e) {
+						System.out.println("=====> has been some error in the server");
+						client.shutdown();
+						return -1;
+					}
+					
+				} else {
+					System.out.println("failed 3 " + future.failedReason());
+				}
+			}
+			
 			client.shutdown();
 			return -1;
+		} catch (Exception e) {
+			System.out.println("Can't find the host");
+			return -1;
 		}
-		
-		if (futureDiscover.isSuccess()) {
-			System.out.println("found that my outside address is " + futureDiscover.peerAddress());
-		} else {
-			System.out.println("failed " + futureDiscover.failedReason());
-		}
-		
-		PeerAddress peerA = addressList.iterator().next();
-		
-		byte[] msg = objectToByteArray(msgObject);
-		
-		
-		if (msgObject.getType() == 1) {
-			// Send and logic to process msg type one
-			FutureDirect future = client.sendDirect(peerA).object(msg).start();
-			
-			future.awaitUninterruptibly();
-			
-			if (future.isSuccess()) {
-				
-				System.out.println("=====> receiving message");
-				int val = (int) future.object();
-				if (val == 1) {
-					// Success!!
-					System.out.println("=====> File part correctly sent");
-					client.shutdown();
-					return val;
-				} else {
-					// Fail in the server (can't save for space, permits, doesn't find, etc)
-					System.out.println("=====> has been some error in the server");
-					client.shutdown();
-					return val;
-				}
-				
-			} else {
-				System.out.println(future.failedReason());
-			}
-		}
-		
-		if (msgObject.getType() == 0) {
-			// Send and logic to process msg type zero
-			FutureDirect future = client.sendDirect(peerA).object(msg).start();
-			
-			future.awaitUninterruptibly();
-			
-			if (future.isSuccess()) {
-				System.out.println("=====> receiving message");
-				try{
-					byte[] byteArray = (byte[]) future.object();
-					FileOutputStream fos = new FileOutputStream(msgObject.getHash()+".aes256");
-					fos.write(byteArray);
-					fos.close();
-					
-					System.out.println("=====> File part recovered");
-					client.shutdown();
-					return 1;
-				} catch (Exception e) {
-					System.out.println("=====> has been some error in the server");
-					client.shutdown();
-					return -1;
-				}
-				
-			} else {
-				System.out.println(future.failedReason());
-			}
-		}
-		
-		client.shutdown();
-		return -1;
 	}
 	
 	
@@ -281,5 +348,25 @@ public class ClientImpl implements Client {
 		os.writeObject(object);
 		os.close();
 		return bs.toByteArray();
+	}
+	
+	/**
+	 * This procedure delete the key file and the encrypt files which can't be delete
+	 * 
+	 * @param String filePath
+	 */
+	private void cleanAllTmpFile(String filePath) {
+		List<NautilusKey> nautilusKey = keysHandler.getKeys(new File(filePath).getName()+"_key.xml");
+		
+		// Delete the keyFile
+		String fileKey = (new File(filePath).getName()) + "_key.xml";
+		new File(fileKey).delete();
+		
+		// Delete the encrypt files
+		String parentPath = new File(filePath).getParent();
+		
+		for (NautilusKey key : nautilusKey) {
+			new File(parentPath + "/" +key.getFileName()).delete();
+		}
 	}
 }
